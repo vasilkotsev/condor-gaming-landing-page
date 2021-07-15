1dea3fd13b1ca26a8fa816c4a05c520f48104d1f
Vasil Kotsev <vasil.kotsev@gmail.com>
2021-07-16 01:22:16 +0300

    adds header, hero and about-us sections.Provides minimum SEO on-page setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,38 @@
 $(document).ready(function () {
-  console.log("ready");
+  /* Hamburger menu functionality */
+  let menu = $(".menu");
+  let logo = $("#logo");
+
+  let header = $("header");
+
+  let hamburger = header.find(".hamburger");
+
+  hamburger.on("click", function () {
+    $(this).toggleClass("hamburger--stand is-active");
+    menu.toggleClass("active");
+    $("html").toggleClass("hidden-scroll");
+    logo.toggleClass("logo-fixed");
+  });
+
+  /*animation scroll functionallity*/
+  $("#hero-section .primary-button")
+    .add("header nav .menu a")
+    .on("click", function (e) {
+      e.preventDefault();
+      let element = $(this).attr("href");
+      let offsetTop = $(element).offset().top;
+      offsetTop = Math.round(offsetTop);
+
+      $("html, body").animate(
+        {
+          scrollTop: offsetTop - 50,
+        },
+        600
+      );
+
+      menu.removeClass("active");
+      $("html").removeClass("hidden-scroll");
+      logo.removeClass("logo-fixed");
+      hamburger.removeClass("hamburger--stand is-active");
+    });
 });
